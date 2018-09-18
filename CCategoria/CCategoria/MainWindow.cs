2c23c642189d5a1dfa69665707979572f85df77b
adrilc24 <adrilc24@gmail.com>
2018-09-18 09:53:39 +0200

    Añadida tarea CMySql con metodo getFieldNames

---
+++ CCategoria/CCategoria/MainWindow.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow : Gtk.Window
+{
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+
+		treeview.AppendColumn("ID", new CellRendererText(), "text", 0);
+		treeview.AppendColumn("Nombre", new CellRendererText(), "text", 1);
+		ListStore listStore = new ListStore(typeof(string), typeof(string));
+		treeview.Model = listStore;
+		listStore.AppendValues("1", "cat 1");
+		listStore.AppendValues("2", "cat 2");
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+}
--- CMysql/CMysql/Program.cs
+++ CMysql/CMysql/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 using MySql.Data.MySqlClient;
 using System.Data;
+using System.Collections.Generic;
 namespace CMysql
 {
 	class MainClass
 	{
-		private static string[] getFielNames(){
-			throw new NotImplementedException();
+		private static string[] getFieldNames(IDataReader dataReader){
+			int fieldCount = dataReader.FieldCount;
+			string[] fieldNames = new string[fieldCount];
+			for (int i = 0; i < fieldCount; i++)
+			{
+				fieldNames[i] = dataReader.GetName(i);
+				Console.WriteLine("Columna = " + fieldNames[i]);
+			}
+			return fieldNames;
+
+			//List<string> fieldNameList = new List<string>();
+			//for (int i = 0; i < fieldCount; i++)
+			//	fieldNameList.Add(dataReader.GetName(i));
+			//return fieldNameList.ToArray;
 		}
         public static void Main(string[] args)
         {
@@ -16,11 +29,27 @@ namespace CMysql
 			dbConnection.Open();
 
 			IDbCommand dbCommand = dbConnection.CreateCommand();
-			dbCommand.CommandText = "select * from categoria order by id";
+			dbCommand.CommandText = "select id as 'Código', nombre as 'Apellido' from categoria order by id";
 			IDataReader dataReader = dbCommand.ExecuteReader();
+			//string[] fieldnames = getFieldNames(dataReader);
+			getFieldNames(dataReader);
+
+
+
+
+
+			//foreach (string fieldname in fieldnames)
+			//{
+			//	Console.WriteLine("Columna = " + fieldname);
+			//}
+			
+			
+			//Console.WriteLine("Número de columnas = " + dataReader.FieldCount);
+			//for (int i = 0; i < dataReader.FieldCount; i++)
+			//	Console.WriteLine("Columna {0} = {1}", i, dataReader.GetName(i));
 			//dataReader.GetName;
-			while (dataReader.Read())
-				Console.WriteLine("id={0} nombre{1}", dataReader["id"], dataReader["nombre"]);
+			//while (dataReader.Read())
+				//Console.WriteLine("id={0} nombre{1}", dataReader["id"], dataReader["nombre"]);
 			    //Console.WriteLine("id={0} nombre{1}", dataReader[0], dataReader[1]);
 			dataReader.Close();
 			dbConnection.Close();
