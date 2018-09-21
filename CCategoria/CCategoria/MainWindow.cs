7393333bec44c0dfc20b99375ebbc927ab5133c7
adrilc24 <adrilc24@gmail.com>
2018-09-21 09:11:16 +0200

    Añadido CCategoria

--- CCategoria/CCategoria/MainWindow.cs
+++ CCategoria/CCategoria/MainWindow.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using Gtk;
+using MySql.Data.MySqlClient;
+using System.Data;
 
 public partial class MainWindow : Gtk.Window
 {
@@ -7,12 +9,30 @@ public partial class MainWindow : Gtk.Window
     {
         Build();
 
+		IDbConnection dbConnection = new MySqlConnection(
+                "server = localhost;database=dbprueba;user=root;password=sistemas;sslmode=none"
+            );
+        dbConnection.Open();
+
 		treeview.AppendColumn("ID", new CellRendererText(), "text", 0);
 		treeview.AppendColumn("Nombre", new CellRendererText(), "text", 1);
+
 		ListStore listStore = new ListStore(typeof(string), typeof(string));
 		treeview.Model = listStore;
-		listStore.AppendValues("1", "cat 1");
-		listStore.AppendValues("2", "cat 2");
+
+		//listStore.AppendValues("1", "cat 1");
+		//listStore.AppendValues("2", "cat 2");
+
+		IDbCommand dbCommand = dbConnection.CreateCommand();
+		dbCommand.CommandText = "select id, nombre from categoria order by id";
+		IDataReader dataReader = dbCommand.ExecuteReader();
+		while (dataReader.Read())
+			listStore.AppendValues(dataReader["id"].ToString(), dataReader["nombre"].ToString());
+		dataReader.Close();
+
+		dbConnection.Close();
+
+
     }
 
     protected void OnDeleteEvent(object sender, DeleteEventArgs a)
