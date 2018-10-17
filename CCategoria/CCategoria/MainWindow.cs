d46cc3a39f4930943f0fa0eb0e4ebb2cfa65bb5f
adrilc24 <adrilc24@gmail.com>
2018-10-17 11:29:47 +0200

    Añadido WindowHelper + cambios en MainWindow

--- CCategoria/CCategoria/MainWindow.cs
+++ CCategoria/CCategoria/MainWindow.cs
@@ -4,26 +4,21 @@ using MySql.Data.MySqlClient;
 using System.Collections.Generic;
 using System.Data;
 using System.Reflection;
+
 using CCategoria;
 using Serpis.Ad;
 
 
 
+
 public partial class MainWindow : Gtk.Window
 {
+
 	public MainWindow() : base(Gtk.WindowType.Toplevel)
 	{
 		Build();
 
-
-
-
-		App.Instance.DbConnection = new MySqlConnection(
-				"server = localhost;database=dbprueba;user=root;password=sistemas;sslmode=none"
-			);
-
-		App.Instance.DbConnection.Open();
-
+		Title = "Categoría";
 
 		TreeViewHelper.Fill(treeview, new string[] { "Id", "Nombre" }, CategoriaDao.Categorias);
 
@@ -31,12 +26,24 @@ public partial class MainWindow : Gtk.Window
 			new CategoriaWindow(new Categoria());
 		};
 
-
+        
 		editAction.Activated += delegate {
-			object id = GetId(treeview);
+			object id = TreeViewHelper.GetId(treeview);
 			Console.WriteLine("Id = " + id);
 			Categoria categoria = CategoriaDao.Load(id);
 			new CategoriaWindow(categoria);
+		};
+
+		deleteAction.Activated += delegate
+		{
+			object id = TreeViewHelper.GetId(treeview);
+            if (WindowHelper.Confirm())
+			    //TODO CategoriaDao.Delete(id);
+
+		};
+
+		refreshAction.Activated += delegate {
+			TreeViewHelper.Fill(treeview, new string[] { "Id", "Nombre" }, CategoriaDao.Categorias);
 		};
 
 
@@ -48,16 +55,6 @@ public partial class MainWindow : Gtk.Window
       
 	}
 
-	public static object GetId(TreeView treeView){
-		return Get(treeView, "Id");
-	}
-
-	public static object Get(TreeView treeView, string propertyName){
-		if (!treeView.Selection.GetSelected(out TreeIter treeIter))
-			return null;
-		object model = treeView.Model.GetValue(treeIter, 0);
-		return model.GetType().GetProperty(propertyName).GetValue(model);
-	}
 
 
 	    private void refreshUI() {
@@ -65,46 +62,9 @@ public partial class MainWindow : Gtk.Window
 		    editAction.Sensitive = treeViewIsSelected;
 		    deleteAction.Sensitive = treeViewIsSelected;
         }
-
-	    
-
-		private void insert()
-		{
-			IDbCommand dbCommand = App.Instance.DbConnection.CreateCommand();
-			dbCommand.CommandText = "insert into categoria (nombre) values ('categoria 4')";
-			int filas = dbCommand.ExecuteNonQuery();
-		}
-
-		private void update()
-		{
-			IDbCommand dbCommand = App.Instance.DbConnection.CreateCommand();
-			dbCommand.CommandText = "update categoria set nombre = 'categoria 4 cambiada modificada' where id = 4";
-			dbCommand.ExecuteNonQuery();
-		}
-
-		private void update(Categoria categoria)
-		{
-			IDbCommand dbCommand = App.Instance.DbConnection.CreateCommand();
-			dbCommand.CommandText = "update categoria set nombre =@nombre where id=@id";
-        
-
-			DbCommandHelper.AddParameter(dbCommand, "nombre", categoria.Nombre);
-			DbCommandHelper.AddParameter(dbCommand, "id", categoria.Id);
-
-
-			dbCommand.ExecuteNonQuery();
-		}
-
-		private void delete()
-		{
-			IDbCommand dbCommand = App.Instance.DbConnection.CreateCommand();
-			dbCommand.CommandText = "delete from categoria where id = 4";
-			dbCommand.ExecuteNonQuery();
-		}
-
+       
 		protected void OnDeleteEvent(object sender, DeleteEventArgs a)
 		{
-			App.Instance.DbConnection.Close();
 			Application.Quit();
 			a.RetVal = true;
 		}
--- CCategoria/CCategoria/Program.cs
+++ CCategoria/CCategoria/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using Gtk;
+using MySql.Data.MySqlClient;
+using Serpis.Ad;
 
 namespace CCategoria
 {
@@ -7,10 +9,18 @@ namespace CCategoria
     {
         public static void Main(string[] args)
         {
+			App.Instance.DbConnection = new MySqlConnection(
+                "server = localhost;database=dbprueba;user=root;password=sistemas;sslmode=none"
+            );
+
+            App.Instance.DbConnection.Open();
+
             Application.Init();
             MainWindow win = new MainWindow();
             win.Show();
             Application.Run();
+
+			App.Instance.DbConnection.Close();
         }
     }
 }
