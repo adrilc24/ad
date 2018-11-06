49f236487efeae75a6fd12551df2fbf531d319ff
adrilc24 <adrilc24@gmail.com>
2018-11-06 08:48:57 +0100

    Modificado dbprueba.sql y añadido CArticulo

--- dbprueba.sql
+++ dbprueba.sql
@@ -8,14 +8,14 @@ insert into categoria (nombre) values ('categoria 2');
 insert into categoria (nombre) values ('categoria 3');
 
 
-create table article(
+create table articulo(
 	id serial primary key,
 	nombre varchar(50) not null unique,
 	precio numeric(10,2) not null,
 	categoria bigint unsigned
 );
 
-alter table articulo add foreignkey categoria references categoria;
+alter table articulo add foreign key (categoria) references categoria(id);
 
 insert into articulo(nombre, precio, categoria) values ('articulo 1', 1.0, 1);
 insert into articulo(nombre, precio, categoria) values ('articulo 2', 2.0, 2);
