a16a24aa452caf1044db1e3b4715e291180d98de
adrilc24 <adrilc24@gmail.com>
2018-11-30 11:34:41 +0100

    Añadido GSerpisConsole

---
+++ GSerpisConsole/src/main/java/serpis/ad/ScannerHelper.java
@@ -0,0 +1,34 @@
+package serpis.ad;
+
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class ScannerHelper {
+	
+	private static Scanner scanner = new Scanner(System.in);
+	
+	public static int getInt(String label) {
+		while(true) {
+			System.out.print(label);
+			String intString = scanner.nextLine();
+			try {
+				return Integer.parseInt(intString);
+			} catch (NumberFormatException ex) {
+				System.out.println("Formato inválido. Vuelva a introducrilo.");
+			}
+		}
+	}
+	
+	
+	public static BigDecimal getBigDecimal(String label) {
+		while(true) {
+			System.out.print(label);
+			String bigDecimalSting = scanner.nextLine();
+			try {
+				return new BigDecimal(bigDecimalSting);
+			} catch (NumberFormatException ex) {
+				System.out.println("Formato inválido. Vuelva a introducrilo.");
+			}
+		}
+	}
+}
