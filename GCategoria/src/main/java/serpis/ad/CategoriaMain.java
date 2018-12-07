22455f57b3ba11ca22c9745fcab9e51597bf1e52
adrilc24 <adrilc24@gmail.com>
2018-12-07 10:42:50 +0100

    Añadidos CategoriaConsole

--- GCategoria/src/main/java/serpis/ad/CategoriaMain.java
+++ GCategoria/src/main/java/serpis/ad/CategoriaMain.java
@@ -2,6 +2,7 @@ package serpis.ad;
 
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.List;
 
 import org.apache.commons.math3.stat.descriptive.StatisticalMultivariateSummary;
 
@@ -15,22 +16,73 @@ public class CategoriaMain {
 		.exitWhen("0 - Salir")
 		.add("1 - Nuevo", CategoriaMain::nuevo)
 		.add("2 - Editar", CategoriaMain::editar)
+		.add("3 - Eliminar", CategoriaMain::eliminar)
 		.loop();		
 		App.getInstance().getConnection().close();
-}
+}	
 	
 	public static void nuevo() {
-		System.out.println("Método nuevo");
+		Categoria categoria = new Categoria();
+		CategoriaConsole.newCategoria(categoria);
+		try {
+			CategoriaDao.save(categoria);
+		} catch (SQLException e) {
+			System.out.println("No se ha podido crear");
+		}
 	}
 	
 	public static void editar() {
-		System.out.println("Método editado");
-		int id = ScannerHelper.getInt("Id: ");
+		long id = CategoriaConsole.getId();
+		Categoria categoria;
+		try {
+			categoria = CategoriaDao.load(id);
+			if (categoria == null) {
+				CategoriaConsole.idNotExists();
+				return;
+			}
+			CategoriaConsole.editCategoria(categoria);
+			CategoriaDao.save(categoria);
+		} catch (SQLException e) {
+			System.out.println("Error al editar");
+			e.printStackTrace();
+		}
+
 	}
 	
-//	public static void salir() {
-//		System.out.println("Hasta Luego");
-//		System.exit(0);
-//	}
-}
+	public static void eliminar() {
+		long id = CategoriaConsole.getId();
+			try {
+				if (CategoriaConsole.deleteConfirm())
+					CategoriaDao.delete(id);
+			} catch (SQLException e) {
+				System.out.println("Error al eliminar");
+			}
+	}
+	
+	public void consultar() {
+		long id = CategoriaConsole.getId();
+		Categoria categoria;
+		try {
+			categoria = CategoriaDao.load(id);
+			if (categoria == null) {
+				CategoriaConsole.idNotExists();
+				return;
+			}
+			CategoriaConsole.show(categoria);
+		} catch (SQLException e) {
+			System.out.println("Error al realizar la consulta");
+		}
 
+	}
+	
+	public static void Listar() {
+		List<Categoria> categorias;
+		try {
+			categorias = CategoriaDao.getAll();
+			CategoriaConsole.showList(categorias);
+		} catch (SQLException e) {
+			System.out.println("Error al obtener lista");
+		}
+
+	}
+}
