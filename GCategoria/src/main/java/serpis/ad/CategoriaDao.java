7ca24ecaccba57b2526625c57b050753215d19ef
adrilc24 <adrilc24@gmail.com>
2018-12-14 10:27:11 +0100

    Actualizado Update e Insert en CategoriDao

--- GCategoria/src/main/java/serpis/ad/CategoriaDao.java
+++ GCategoria/src/main/java/serpis/ad/CategoriaDao.java
@@ -4,15 +4,16 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.math.BigInteger;
 import java.sql.*;
-import java.util.ArrayList;
+import java.util.ArrayList;import org.apache.commons.math3.distribution.GeometricDistribution;
+
 import java.util.*;
 import java.time.*;
 import com.mysql.cj.jdbc.result.UpdatableResultSet;
 import com.mysql.*;
 
 public class CategoriaDao {
-	private static String insertSql = "insert into categoria (nombre) values (?)";
-	private static String updateSql = "update categoria set (nombre) where (id) = (?)";
+	private static String insertSql = "insert into categoria nombre values ?";
+	private static String updateSql = "update categoria set nombre=? where id = ?";
 	private static String deleteSql = "delete * from categoria where id = (?)";
 	private static String selectAll = "select id, nombre from categoria";
 	private static String selectWhereId = "select id, nombre from categoria where id = ?";
@@ -20,13 +21,18 @@ public class CategoriaDao {
 	private static int insert(Categoria categoria) throws SQLException {
 		PreparedStatement preparedStatement = App.getInstance().getConnection().prepareStatement(insertSql);
 		preparedStatement.setObject(1, categoria.getNombre());
-		return preparedStatement.executeUpdate();
+		int rowCount = preparedStatement.executeUpdate();
+		preparedStatement.close();
+		return rowCount;
 	}
 
 	private static int update(Categoria categoria) throws SQLException {
 		PreparedStatement preparedStatement = App.getInstance().getConnection().prepareStatement(updateSql);
 		preparedStatement.setObject(1, categoria.getNombre());
-		return preparedStatement.executeUpdate();
+		preparedStatement.setObject(2, categoria.getId());
+		int rowCount = preparedStatement.executeUpdate();
+		preparedStatement.close();
+		return rowCount;
 	}
 
 	public static int save(Categoria categoria) throws SQLException {
